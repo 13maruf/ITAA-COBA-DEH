5180eae8b3d4c93359503cc2a5adb593bb29d8bb
13maruf <mhmmdmaruf97@gmail.com>
2024-10-15 20:24:01 +0800

    Add files via upload

---
+++ script itaa.js
@@ -0,0 +1,4 @@
+document.getElementById("showBtn").addEventListener("click", function() {
+    const imageContainer = document.querySelector(".image-container");
+    imageContainer.style.opacity = 1; // Menampilkan foto dengan transisi halus
+});
